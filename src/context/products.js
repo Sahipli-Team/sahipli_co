b8d2713fb042f67f8c3b8cf615bb8bd1e4fde969
emrekazanci <emre.kzanci@gmail.com>
2023-03-02 12:03:52 +0300

    added fetchProducts

--- src/context/products.js
+++ src/context/products.js
@@ -6,26 +6,33 @@ const ProductContext = createContext();
 function Provider({ children }) {
   const [products, setProducts] = useState([]);
 
+  const fetchProducts = async () => {
+    const response = await axios.get("http://localhost:3001/products");
+    setProducts(response.data);
+  };
+
   const createProduct = async (
     date,
     title,
     category,
     price,
     description,
-    image) => {
-      const response = await axios.post('http://localhost:3001/products',{
-        date,
-        title,
-        category,
-        price,
-        description,
-        image
-      })
-      const updatedProducts = [...products, response.data]
-      setProducts(updatedProducts)
-    }
+    image
+  ) => {
+    const response = await axios.post("http://localhost:3001/products", {
+      date,
+      title,
+      category,
+      price,
+      description,
+      image,
+    });
+    const updatedProducts = [...products, response.data];
+    setProducts(updatedProducts);
+  };
   const valueToShare = {
     products,
+    fetchProducts,
     createProduct,
   };
   return (
