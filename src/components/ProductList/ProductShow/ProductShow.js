0847a917fef3922379559304c961a6576c11e532
emrekazanci <emre.kzanci@gmail.com>
2023-03-02 12:08:26 +0300

    added variables

--- src/components/ProductList/ProductShow/ProductShow.js
+++ src/components/ProductList/ProductShow/ProductShow.js
@@ -1,6 +1,11 @@
+import { useContext, useState } from "react";
 import ProductEdit from "./ProductEdit/ProductEdit";
+import ProductContext from "../../../context/products";
 
-const ProductShow = () => {
+const ProductShow = ({ product }) => {
+  const { title, category, price, image, description, date } = product;
+  const [showEdit, setShowEdit] = useState(false);
+  const { deleteProductById } = useContext(ProductContext);
   return (
     <>
       <div>
