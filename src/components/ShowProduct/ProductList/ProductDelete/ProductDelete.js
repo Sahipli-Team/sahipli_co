e31ad83e784f680856989cd84e23f1e69d49d924
emrekazanci <emre.kzanci@gmail.com>
2023-03-02 10:20:18 +0300

    refactor file name

--- src/components/ShowProduct/ProductList/ProductDelete/ProductDelete.js
+++
@@ -1,9 +0,0 @@
-import DeleteImageButton from "../../../../Image/deleteImage.png";
-const ProductDelete = () => {
-  return (
-    <button>
-      <img src={DeleteImageButton} alt="Delete Image Button" />
-    </button>
-  );
-};
-export default ProductDelete;
