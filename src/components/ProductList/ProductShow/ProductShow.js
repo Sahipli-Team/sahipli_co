e5aec420f669f9826e26b7d30360d79b63121f0e
emrekazanci <emre.kzanci@gmail.com>
2023-03-02 12:13:50 +0300

    handleEditClick function has been added

--- src/components/ProductList/ProductShow/ProductShow.js
+++ src/components/ProductList/ProductShow/ProductShow.js
@@ -1,17 +1,32 @@
 import { useContext, useState } from "react";
 import ProductEdit from "./ProductEdit/ProductEdit";
 import ProductContext from "../../../context/products";
+import DeleteImageButton from "../../../Image/deleteImage.png";
+import EditImageButton from "../../../Image/EditImage.png";
 
 const ProductShow = ({ product }) => {
   const { title, category, price, image, description, date } = product;
   const [showEdit, setShowEdit] = useState(false);
   const { deleteProductById } = useContext(ProductContext);
+
+  const handleEditClick = () => {
+    setShowEdit((p) => !p);
+  };
+
   return (
     <>
       <div>
-        <ProductEdit />
+        <button>
+          <img src={DeleteImageButton} alt="Delete Image Button" />
+        </button>
+        <button>
+          <img
+            src={EditImageButton}
+            alt="Edit Image Button"
+            onClick={handleEditClick}
+          />
+        </button>
       </div>
-      <div></div>
     </>
   );
 };
