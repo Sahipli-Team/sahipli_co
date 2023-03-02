d26c4f23878e8d856ee1c07d7d675eedeb215f5c
mucahitidogan <76558268+mucahitidogan@users.noreply.github.com>
2023-03-02 11:21:54 +0300

    createProduct function added to product.js

--- src/context/products.js
+++ src/context/products.js
@@ -6,7 +6,28 @@ const ProductContext = createContext();
 function Provider({ children }) {
   const [products, setProducts] = useState([]);
 
-  const valueToShare = {};
+  const createProduct = async (
+    date,
+    title,
+    category,
+    price,
+    description,
+    image) => {
+      const response = await axios.post('http://localhost:3001/products',{
+        date,
+        title,
+        category,
+        price,
+        description,
+        image
+      })
+      const updatedProducts = [...products, response.data]
+      setProducts(updatedProducts)
+    }
+  const valueToShare = {
+    products,
+    createProduct,
+  };
   return (
     <ProductContext.Provider value={valueToShare}>
       {children}
