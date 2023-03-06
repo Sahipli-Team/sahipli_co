9c7d201eebb3a0b3eb8186a3c357885dfc841b5e
mucahitidogan <76558268+mucahitidogan@users.noreply.github.com>
2023-03-06 15:05:23 +0300

    search css has been updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,29 @@
 import Image from "../../Image/Slogo.png";
 import ProductContext from "../../context/products";
 import { useContext } from "react";
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+  integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+  crossorigin="anonymous"
+  referrerpolicy="no-referrer"
+/>;
 const Header = () => {
   const { setSearch } = useContext(ProductContext);
 
   return (
     <div className="header">
       <img src={Image} alt="S title" />
-      <form action="" autocomplete="on">
+      <div className="search-box">
+        <button className="btn-search">
+          <i class="fas fa-search"></i>
+        </button>
         <input
-          className="search"
+          className="input-search"
           placeholder="Search product..."
           onChange={(e) => setSearch(e.target.value)}
         ></input>
-        <input type="submit" className="search_submit" />
-      </form>
+      </div>
     </div>
   );
 };
