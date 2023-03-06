c07935ff8fb9851671a7dba71418c13752b93bd0
mucahitidogan <76558268+mucahitidogan@users.noreply.github.com>
2023-03-06 14:11:00 +0300

    serchBar updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,23 @@
 import Image from "../../Image/Slogo.png";
 import ProductContext from "../../context/products";
 import { useContext } from "react";
+import search from "../../Image/search.png";
 const Header = () => {
   const { setSearch } = useContext(ProductContext);
 
   return (
     <div className="header">
       <img src={Image} alt="S title" />
-      <form action="" autocomplete="on">
+      <div className="search-box">
+        <button className="btn-search">
+          <img src={search} alt="search image" />
+        </button>
         <input
-          className="search"
+          className="input-search"
           placeholder="Search product..."
           onChange={(e) => setSearch(e.target.value)}
         ></input>
-        <input type="submit" className="search_submit" />
-      </form>
+      </div>
     </div>
   );
 };
