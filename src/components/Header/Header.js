1339bd0f7568ddb3d23d2c089c1522739e8cd38b
Tunç Cenk Ergüven <erguven.cenk@gmail.com>
2023-03-06 12:04:18 +0300

    searchBar created

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,16 @@
 import Image from "../../Image/Slogo.png";
-
+import ProductContext from "../../context/products";
+import { useContext } from "react";
 const Header = () => {
+  const { setSearch } = useContext(ProductContext);
+
   return (
     <div className="header">
       <img src={Image} alt="S title" />
-      <input ></input>
+      <input
+        placeholder="Search..."
+        onChange={(e) => setSearch(e.target.value)}
+      ></input>
     </div>
   );
 };
