47cd72d514242a06a3e33d5e9aca7420b9bad3dd
Tunç Cenk Ergüven <erguven.cenk@gmail.com>
2023-03-09 13:23:41 +0300

    ProductList comment lines added

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -3,10 +3,14 @@ import ProductContext from "../../context/products";
 import { useContext } from "react";
 
 const ProductList = () => {
+  //filtered products pulled from useContext
   const { products } = useContext(ProductContext);
+  //productList created to hold all the products which will be shown as cards
   const renderedProducts = products.map((product) => {
     return <ProductShow key={product.id} product={product} />;
   });
+  //html format of productList
   return <div>{renderedProducts}</div>;
 };
+//ProductList exported here
 export default ProductList;
